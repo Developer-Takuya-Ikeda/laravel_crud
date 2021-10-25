67da1a1951b36a45e3a38930207b2fefd584dd19
Takuya.Ikeda <developer.takuya.ikeda@gmail.com>
2021-10-25 10:09:29 +0900

    * Create Controller * Create View

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\UserController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+// Users
+// Route::resource('users', [App\Http\Controllers\QandAController::class, 'update''UserController');
+// 上記のresourceの内容を個別に定義した場合.
+Route::get('/users', [UserController::class, 'index'])->name('users.index');
+Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+Route::post('/users', [UserController::class, 'store'])->name('users.store');
+    
+Route::get('/users/{user}', [UserController::class, 'show'])->name('users.show');
+Route::get('/users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
+Route::post('/users/{user}', [UserController::class, 'update'])->name('users.update');// PUTよりはPOSTの方が良い.
+Route::delete('/users/{user}', [UserController::class, 'v'])->name('users.destroy');
